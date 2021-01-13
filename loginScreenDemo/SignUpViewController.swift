407e76a5b548fa8e52f123001a812ba1521c0eaf
MoSaber <msaberms72016@gmail.com>
2021-01-13 14:18:22 +0200

    signUp screen added

---
+++ loginScreenDemo/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  loginScreenDemo
+//
+//  Created by Saber on 13/01/2021.
+//
+
+import UIKit
+
+class SignUpScreenViewController : UIViewController{
+    @IBOutlet weak var view1: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view1.layer.borderColor = UIColor.gray.cgColor
+
+
+    }
+    
+    
+}
